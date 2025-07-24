20d73921de772f6a9e02953829b2147b748e515e
victxr_dev <victorjesusibarragonzalez@gmail.com>
2025-07-24 16:58:17 -0300

    Estructura final con identacion y bloque __main__

--- main.py
+++ main.py
@@ -13,37 +13,39 @@ def show_menu():
 def main():
     book = ContactBook()
 
-while True:
-    show_menu()
-    choice = input("Select an option: ")
+    while True:
+        show_menu()
+        choice = input("Select an option: ")
+        if choice == "1":
+            name = input("Name: ")
+            phone = input("Phone: ")
+            email = input("Email: ")
+            book.add_contact(name, phone, email)
+            print("Contact added successfully.")
 
-    if choice == "1":
-        name = input("Name: ")
-        phone = input("Phone: ")
-        email = input("Email: ")
-        book.add_contact(name, phone, email)
-        print("Contact added successfully.")
+        elif choice == "2":
+            print("Contacts list:")
+            print("\n")
+            book.list_contacts()
 
-    elif choice == "2":
-        print("Contacts list:")
-        print("\n")
-        book.list_contacts()
+        elif choice == "3":
+            name = input("Name to search: ")
+            contact = book.find_contact(name)
+            if contact:
+                print("Contact found:", contact)
+            else:
+                print("Contact not found")        
 
-    elif choice == "3":
-        name = input("Name to search: ")
-        contact = book.find_contact(name)
-        if contact:
-            print("Contact found:", contact)
-        else:
-            print("Contact not found")        
+        elif choice == "4":
+            name = input("Name to delete: ")
+            if book.delete_contact(name):
+                print("Contact successfully deleted.")
+            else:
+                print("Contact not found.")
 
-    elif choice == "4":
-        name = input("Name to delete: ")
-        if book.delete_contact(name):
-            print("Contact successfully deleted.")
-        else:
-            print("Contact not found.")
+        elif choice == "5":
+            print("Leaving the contact manager. ¡See you later!")
+            break
 
-    elif choice == "5":
-        print("Leaving the contact manager. ¡See you later!")
-        break
+if __name__ == "__main__":
+    main()
