7f4c85574006676fb7cc10a15a16b165844eb62f
victxr_dev <victorjesusibarragonzalez@gmail.com>
2025-07-24 10:05:05 -0300

    Agregar método list_contact

--- contact_book.py
+++ contact_book.py
@@ -7,3 +7,10 @@ class ContactBook:
     def add_contact(self, name, phone, email):
         contact = Contact(name, phone, email)
         self.contacts.append(contact)
+    
+    def list_contacts(self):
+        if not self.contacts:
+            print("There aren't contacts registered")
+        else:
+            for i, contact in enumerate(self.contacts, start=1):
+                print(f"{i}. {contact}")
