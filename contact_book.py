a21b6f872a7aab8b94130178de3a3e7cc4e2229b
victxr_dev <victorjesusibarragonzalez@gmail.com>
2025-07-24 09:54:41 -0300

    Agregar método add_contact

--- contact_book.py
+++ contact_book.py
@@ -3,3 +3,7 @@ from contact import Contact
 class ContactBook:
     def __init__(self):
         self.contacts = []
+
+    def add_contact(self, name, phone, email):
+        contact = Contact(name, phone, email)
+        self.contacts.append(contact)
