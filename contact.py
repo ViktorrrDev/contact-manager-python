b0d6de988b122ce2ef22be9b30236c216e7941c4
victxr_dev <victorjesusibarragonzalez@gmail.com>
2025-07-24 09:39:02 -0300

    Agregando el método __str__ para mostrar contactos

--- contact.py
+++ contact.py
@@ -3,4 +3,6 @@ class Contact:
         self.name = name
         self.phone = phone
         self.email = email
-        
+    
+    def __str__(self):
+        return f"{self.name} | {self.phone} | {self.email}"
